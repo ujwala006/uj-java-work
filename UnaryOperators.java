028584a0a28cfb885baa66bc26003886caa16181
Ujwala Umamaheshwari K K <ujumamaheshwari06@gmail.com>
2026-03-25 15:24:06 +0530

    Add files via upload

---
+++ UnaryOperators.java
@@ -0,0 +1,16 @@
+public class UnaryOperators {
+    public static void main(String[] args){
+        int x=5;
+        System.out.println("original "+x);
+        System.out.println("post increment "+ (x++));
+        System.out.println("after incrementing "+ x);
+        System.out.println("pre increment "+ (++x));
+        System.out.println("after pre increment "+ x);
+        System.out.println("pre decrement "+(--x));
+        System.out.println("after pre decrement "+x);
+        System.out.println("post decrement "+(x--));
+        System.out.println("after post decrement "+x);
+    }
+        
+    }
+
