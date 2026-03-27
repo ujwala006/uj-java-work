a6fa0ffcf748f88f32ed2b3d07d92e79cf011e14
Ujwala Umamaheshwari K K <ujumamaheshwari06@gmail.com>
2026-03-27 14:39:33 +0530

    Add files via upload

---
+++ PrintArrayUsingNestedForLoop.java
@@ -0,0 +1,15 @@
+public class PrintArrayUsingNestedForLoop {
+    public static void main(String[] args) {
+        for (int i=1;i<=2;i++){
+            System.out.println("tiger");
+            for (int j=1;j<=2;j++){
+                System.out.println("    lion");}
+                for (int k=1;k<=3;k++){
+                    System.out.println("cat");
+                }
+
+        }
+        }
+    }
+
+
