23c2755fe130abc2d7135f974390b270231b6865
Ujwala Umamaheshwari K K <ujumamaheshwari06@gmail.com>
2026-03-27 10:36:31 +0530

    Add files via upload

---
+++ Switchcase.java
@@ -0,0 +1,26 @@
+public class Switchcase {
+    public static void main(String[] args) {
+        int day=2;
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("WEdnesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            default:
+                System.out.println("Invalid day");
+
+        }
+    }
+    
+}
